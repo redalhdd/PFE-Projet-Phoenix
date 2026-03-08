128caf95ef14c7d915816bb02d403c8a8bca2d34
Clement Nassih <clement.nassih@etu-uca.fr>
2026-03-08 21:28:49 +0100

    debug path et bash

--- run_outils.sh
+++ run_outils.sh
@@ -3,15 +3,15 @@
 c_file="$1"
 
 # run scheduler.sh et récupère le nom du json en output
-graphe_json=$(./scheduling/scheduler.sh "$c_file")
+json_schedule=$(./scheduling/scheduler.sh "$c_file" | tail -n 1)
 if [ $? -ne 0 ]; then
     echo "ERREUR : scheduler.sh a échoué" >&2
     exit 1
 fi
 
-echo "JSON généré : $graphe_json"
+# echo "JSON généré : $graphe_json"
 
-./Generateur_VHDL/run.sh "$graphe_json"
+./Generateur_VHDL/run.sh "$json_schedule"
 if [ $? -ne 0 ]; then
     echo "ERREUR : run.sh a échoué" >&2
     exit 1
--- scheduling/scheduler.sh
+++ scheduling/scheduler.sh
@@ -13,14 +13,13 @@ IR_file="${base}.ll"
 clang -S -emit-llvm -Xclang -disable-O0-optnone "$c_file" -o - | opt -passes='mem2reg' -S -o "$IR_file"
 
 # génération du json représentant le grapphe de flot et de dépendance de l'IR LLVM
-python3 ./scheduling/scripts/dag_block_v4.py "$IR_file" > "${base}_graphe.json"
+python3 scheduling/scripts/dag_block_v4.py "$IR_file" > "${base}_graphe.json"
 
 # visualisation du graphe de flot et de dépendance avec graphviz
-python3 ./scheduling/scripts/visu_dag.py "${base}_graphe.json"
+python3 scheduling/scripts/visu_dag.py "${base}_graphe.json"
 
 #list scheduling des opérations présente dans le json de dépendance/flot
-python3 ./scheduling/scripts/scheduler_cycle_html_v3.py "${base}_graphe.json"
+python3 scheduling/scripts/scheduler_cycle_html_v3.py "${base}_graphe.json"
 
 # deduction du nom du json de scheduling a donné en paramètre à  l'outil de reda
-json_schedule="${base}_graphe_schedule.json"
-echo "Fichier JSON schedule : $json_schedule"
+echo "${base}_graphe_schedule.json"
